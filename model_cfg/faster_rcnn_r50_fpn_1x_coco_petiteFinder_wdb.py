2bd5863b7be356ac70335bb8e2fdb94d591402c6
Eugene Klyshko <klyshko.jack@gmail.com>
2022-01-07 13:08:48 -0500

    Simplified the config file

--- model_cfg/faster_rcnn_r50_fpn_1x_coco_petiteFinder_wdb.py
+++ model_cfg/faster_rcnn_r50_fpn_1x_coco_petiteFinder_wdb.py
@@ -21,7 +21,7 @@ model = dict(
         feat_channels=256,
         anchor_generator=dict(
             type='AnchorGenerator',
-            scales=[8],
+            scales=[4, 8],
             ratios=[0.5, 1.0, 2.0],
             strides=[4, 8, 16, 32, 64]),
         bbox_coder=dict(
@@ -101,20 +101,23 @@ model = dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
             max_per_img=100)))
-dataset_type = 'COCODataset'
-data_root = 'data/coco/'
+
+dataset_type = 'CocoDataset'
+data_root = '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/'
+
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+img_scale = (512, 512)
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(
         type='Normalize',
-        mean=[123.675, 116.28, 103.53],
-        std=[58.395, 57.12, 57.375],
-        to_rgb=True),
+        **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
@@ -123,98 +126,44 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(800, 800),
+        img_scale=img_scale,
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(
                 type='Normalize',
-                mean=[123.675, 116.28, 103.53],
-                std=[58.395, 57.12, 57.375],
-                to_rgb=True),
+                **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img'])
         ])
 ]
+
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
+    
     train=dict(
-        type='CocoDataset',
-        ann_file=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_train/512_02/train_sliced_512.json',
-        img_prefix=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_train/512_02/train_sliced_images_512/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
-            dict(type='RandomFlip', flip_ratio=0.5),
-            dict(
-                type='Normalize',
-                mean=[123.675, 116.28, 103.53],
-                std=[58.395, 57.12, 57.375],
-                to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-        ],
-        classes=('g', 'p')),
+        type=dataset_type,
+        ann_file = data_root + 'slice_coco_train/512_02/train_sliced_512.json',
+        img_prefix = data_root + 'slice_coco_train/512_02/train_sliced_images_512/',
+        pipeline=train_pipeline),
+    
     val=dict(
-        type='CocoDataset',
-        ann_file=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_validate/512_02/validate_sliced_512.json',
-        img_prefix=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_validate/512_02/validate_sliced_images_512/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(800, 800),
-                flip=True,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Normalize',
-                        mean=[123.675, 116.28, 103.53],
-                        std=[58.395, 57.12, 57.375],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='ImageToTensor', keys=['img']),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ],
-        classes=('g', 'p')),
+        type=dataset_type,
+        ann_file = data_root + 'slice_coco_validate/512_02/validate_sliced_512.json',
+        img_prefix = data_root + 'slice_coco_validate/512_02/validate_sliced_images_512/',
+        pipeline=test_pipeline),
+    
     test=dict(
-        type='CocoDataset',
-        ann_file=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_test/512_02/test_sliced_512.json',
-        img_prefix=
-        '/home/klyshko/Desktop/petiteFinder/COCO_dataset_sliced/slice_coco_test/512_02/test_sliced_images_512/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(800, 800),
-                flip=True,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Normalize',
-                        mean=[123.675, 116.28, 103.53],
-                        std=[58.395, 57.12, 57.375],
-                        to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
-                    dict(type='ImageToTensor', keys=['img']),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ],
-        classes=('g', 'p')))
+        type=dataset_type,
+        ann_file = data_root + 'slice_coco_test/512_02/test_sliced_512.json',
+        img_prefix = data_root + 'slice_coco_test/512_02/test_sliced_images_512/',
+        pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
+
+
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(
@@ -223,7 +172,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=50)
+runner = dict(type='EpochBasedRunner', max_epochs=20)
 checkpoint_config = dict(interval=1)
 
 custom_hooks = [dict(type='NumClassCheckHook')]
@@ -235,21 +184,13 @@ workflow = [('train', 1), ('val', 1)]
 classes = ('g', 'p')
 gpu_ids = range(0, 1)
 
+
 project_name = 'petiteFinder'
-name = 'experiment_1'
+name = 'exp_500px_4-8anchors'
 work_dir = '/media/klyshko/HDD/ML_runs/{}/{}'.format(project_name, name)
 
 log_config = dict(
     interval=50,
-    hooks = [
-        dict(type='TextLoggerHook'),
-        dict(
-            type='WandbLoggerHook',
-            init_kwargs=dict(
-                project=project_name,
-                name=name
-                )
-            )
-        ]
+    hooks = [ dict(type='TextLoggerHook'), dict(type='WandbLoggerHook', init_kwargs=dict(project=project_name,name=name))]
     )
 
